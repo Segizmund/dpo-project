03715c6391dff6b93a4eca6fcc1b31efdb7f882e
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2026-04-21 14:13:48 +0300

    21.04

--- resources/js/Components/DynamicSection.jsx
+++ resources/js/Components/DynamicSection.jsx
@@ -1,42 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import {Link } from '@inertiajs/react';
 
-export default function DynamicSection({activeSection, sectionContent}) {
+export default function DynamicSection({activeSection, sectionContent = []}) {
     const [content, setContent] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     console.log(sectionContent)
 
-    function rangeDate(startDate, endDate){
-       startDate = startDate.split('T').slice(0);
-       startDate = startDate[0].split('-');
-       endDate = endDate.split('T').slice(0);
-       endDate = endDate[0].split('-');
-       
-       console.log(startDate);
-       console.log('-');
-       console.log(endDate);
+    const dataToRender = Array.isArray(sectionContent) 
+    ? sectionContent 
+    : (sectionContent?.data || []);
+
+    function rangeDate(startDate, endDate) {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+
+        const diffInMs = end - start;
+
+        const weeks = Math.floor(diffInMs / (7 * 24 * 60 * 60 * 1000));
+
+        const months = (end.getFullYear() - start.getFullYear()) * 12 + (end.getMonth() - start.getMonth());
+
+        
+        return `${months} мес. (${weeks > 0 ? weeks + 'нед.': null})`;
     }
 
     return (
         <div className=" animate-fadeIn">
             <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-2.5">
-                {sectionContent.map((item) => (
-                        <Link key={item.id} className='bg-[#EFEFEF] p-6 rounded-3xl flex flex-col gap-12 hover:-translate-y-1 hover:opacity-90 transition duration-300 ease-linear'>
-                            <div className='flex justify-between'>
-                                <div>
-                                    <span>Курс</span>
+                {sectionContent.length > 0 ? (
+                        sectionContent?.map((item) => (
+                            <Link key={item.id} className='bg-[#EFEFEF] p-6 rounded-3xl flex flex-col gap-12 hover:-translate-y-1 hover:opacity-90 transition duration-300 ease-linear'>
+                                <div className='flex justify-between'>
+                                    <div>
+                                        <span>Курс</span>
+                                    </div>
+                                    <div className='rounded-3xl'>
+                                        <img className='rounded-3xl object-cover h-[170px] w-[170px]' src="/img/course.png" alt="" />
+                                    </div>
                                 </div>
-                                <div className='rounded-3xl'>
-                                    <img className='rounded-3xl object-cover h-[170px] w-[170px]' src="/img/course.png" alt="" />
+                                <div className='flex flex-col gap-5 justify-between h-full'>
+                                    <h4 className='font-bold text-2xl'>{item.name}</h4>
+                                    <span className='opacity-60'>{rangeDate(item.start_date, item.end_date)}</span>
                                 </div>
-                            </div>
-                            <div className='flex flex-col gap-5'>
-                                <h4 className='font-bold text-2xl'>{item.name}</h4>
-                                <span className='opacity-60'>{rangeDate(item.start_date, item.end_date)}</span>
-                            </div>
-                        </Link>
-                    ))
+                            </Link>
+                        ))
+                    ) 
+                    : 
+                    (
+                        <div className='flex justify-center col-span-full'>
+                            <span className='font-bold text-2xl'>В данный момент ничего нет.</span>
+                        </div>
+                    )
                 }
             </div>
             <div className='flex justify-center mt-12'>
